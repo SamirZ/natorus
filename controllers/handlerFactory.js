a2778d6470e7b4b0ec81bd2f5c47bb9d08862de4
SamirZ <z.samir@live.com>
2019-10-21 18:46:42 +0200

    Removed explain from get all tours

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -80,7 +80,7 @@ exports.getAll = Model =>
       .sort()
       .limitFileds()
       .paginate();
-    const doc = await features.query.explain();
+    const doc = await features.query;
 
     res.status(200).json({
       status: 'success',
